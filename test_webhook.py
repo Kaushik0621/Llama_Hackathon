77f644127a75fbce46c23ae2403b0485e4430332
Recklesz <gitcupid@gmail.com>
2024-11-17 07:09:38 +0000

    change the address of the test webhook

--- test_webhook.py
+++ test_webhook.py
@@ -2,7 +2,9 @@ import requests
 import json
 
 # Webhook URL (assuming Flask is running locally on port 5001)
-webhook_url = "http://localhost:5001/webhook/create_user"
+# webhook_url = "http://localhost:5001/webhook/create_user"
+webhook_url = "https://llama-hackathon-heroku-ac0409d236cb.herokuapp.com/webhook/create_user"
+
 
 # Sample user data matching the required fields
 test_data = {
